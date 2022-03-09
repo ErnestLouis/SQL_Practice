6e82e473832d2eadb4a90eecf721dc27dd2f806c
Ernest <53148524+ErnestLouis@users.noreply.github.com>
2022-03-09 13:06:40 -0500

    Create Create_Insert.sql

---
+++ Create_Insert.sql
@@ -0,0 +1,46 @@
+--CREATE TABLE Person
+--(person_id int,
+--first_name Varchar(50),
+--last_name Varchar(50),
+--gender Varchar(7),
+--age int,
+--primary key(person_id)
+--)
+
+--CREATE TABLE Career
+--(person_id int,
+--job_title Varchar(50),
+--salary Varchar(50),
+--primary key(person_id)
+--)
+
+--Insert values into tables (1)
+
+--INSERT INTO Person VALUES 
+--( 001, 'Ben', 'Carson', 'Male', 70),
+--( 002, 'Elon', 'Musk', 'Male', 50),
+--( 003, 'Barbara', 'ONeill', 'Female', 68),
+--( 004, 'J.K.', 'Rowling', 'Female', 56),
+--( 005, 'Warren ', 'Buffett', 'Male', 91),
+--( 006, 'Doug', 'Batchelor', 'Male', 65),
+--( 007, 'Harry', 'Lorayne', 'Male', 95),
+--( 008, 'Lauren', 'Daigle', 'Female', 30),
+--( 009, 'Kobe ', 'Bryant', 'Male', 41),
+--( 010, 'Arnold ', 'Schwarzenegger', 'Male', 74)
+
+
+--Insert values into tables (Career)
+
+--INSERT INTO Career VALUES
+--(001, 'Neurosurgeon', 634101),
+--(002, 'Entrepreneur', 360000),
+--(003, 'naturopath', 155000),
+--(004, 'Writer', 360000),
+--(005, 'Investor', 545000),
+--(006, 'Pastor', 136000),
+--(007, 'Memory-Trainer', 120000),
+--(008, 'Singer', 126000),
+--(009, 'Basketball Player', 400000),
+--(010, 'BodyBuilder', 436000)
+
+--SELECT * FROM Person
